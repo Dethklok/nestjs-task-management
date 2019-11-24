8bb2100332a128591e757f111d67b608eba4f6f1
igor <x.firedragon@gmail.com>
2019-11-24 18:57:41 +0500

    Persistence: Getting Tasks (with or without filters)

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -11,15 +11,11 @@ export class TasksController {
   constructor(private tasksService: TasksService) {
   }
 
-  // @Get()
-  // getTasks(@Query(ValidationPipe) filterDto: GetTasksFilterDto): Task[] {
-  //   if (Object.keys(filterDto).length > 0) {
-  //     return this.tasksService.getTasksWithFilter(filterDto);
-  //   } else {
-  //     return this.tasksService.getAllTasks();
-  //   }
-  // }
-  //
+  @Get()
+  getTasks(@Query(ValidationPipe) filterDto: GetTasksFilterDto): Promise<Task[]> {
+    return this.tasksService.getTasks(filterDto);
+  }
+
   @Get('/:id')
   getTaskById(@Param('id', ParseIntPipe) id: number): Promise<Task> {
     return this.tasksService.getTaskById(id);
