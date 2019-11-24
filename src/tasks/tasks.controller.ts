c8fa50ea054664101ab50f4b059d340fb385fbe9
igor <x.firedragon@gmail.com>
2019-11-24 17:44:52 +0500

    Persistence: Delete Task

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -31,11 +31,11 @@ export class TasksController {
     return this.tasksService.createTask(createTaskDto);
   }
 
-  // @Delete('/:id')
-  // deleteTask(@Param('id') id: string): void {
-  //   return this.tasksService.deleteTask(id);
-  // }
-  //
+  @Delete('/:id')
+  deleteTask(@Param('id', ParseIntPipe) id: number): Promise<void> {
+    return this.tasksService.deleteTask(id);
+  }
+
   // @Patch('/:id/status')
   // updateStatus(
   //   @Param('id') id: string,
