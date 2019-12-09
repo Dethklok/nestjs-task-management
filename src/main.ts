2c884179e7d908c559b70221ff2791d91cf61b19
igor <x.firedragon@gmail.com>
2019-12-09 20:45:54 +0500

    Configuration management Set-up

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import * as config from 'config';
 
 async function bootstrap() {
   const logger = new Logger('bootstrap');
+
+  const serverConfig = config.get('server');
+  console.log(serverConfig);
 
   const port = 3000;
   const app = await NestFactory.create(AppModule);
